ef016642ac745f74a6e37bcc02716d9cd65b2dea
jeffrey82221 <jeffrey82221@gmail.com>
2025-07-10 01:51:22 +0800

    fix: test

--- test_celery_multi_workers/run_task.sh
+++ test_celery_multi_workers/run_task.sh
@@ -1,2 +1,2 @@
-celery -A tasks worker --concurrency=1 -Q queue_a -n worker_a@%h
-celery -A tasks worker --concurrency=1 -Q queue_b -n worker_b@%h
+celery -A tasks worker --concurrency=1 -Q queue_a -n worker_a@%h -O fair
+celery -A tasks worker --concurrency=1 -Q queue_b -n worker_b@%h -O fair
--- test_celery_v3/run_task.sh
+++ test_celery_v3/run_task.sh
@@ -1,1 +1,1 @@
-celery -A tasks worker --loglevel=info --concurrency=1
+celery -A tasks worker --loglevel=info --concurrency=1 -O fair
