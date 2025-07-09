22c5bbe22ebbd9832a9cf387cc4b9fc21b1f102d
jeffrey82221 <jeffrey82221@gmail.com>
2025-07-09 23:36:12 +0800

    refactor: tune

--- test_kq/worker1.py
+++ test_kq/worker1.py
@@ -2,14 +2,6 @@ import logging
 from kafka import KafkaConsumer
 from kq import Worker
 
-# Set up logging
-formatter = logging.Formatter("[%(levelname)s] %(message)s")
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger = logging.getLogger("kq.worker")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(stream_handler)
-
 # Set up Kafka consumer
 consumer = KafkaConsumer(
     bootstrap_servers="127.0.0.1:9092",
